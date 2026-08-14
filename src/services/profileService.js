8530ff32ef75e4c8ae83a697c7274f42dba25897
kayalvizhi-28 <kayalvizhisathya2@gmail.com>
2026-08-14 18:38:22 +0530

    Fix profile API endpoint

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -2,15 +2,15 @@ import api from './api';
 
 export const profileService = {
   getAll: async () => {
-    const response = await api.get('/profile');
+    const response = await api.get('/profiles');
     return response.data;
   },
   create: async (profile) => {
-    const response = await api.post('/profile', profile);
+    const response = await api.post('/profiles', profile);
     return response.data;
   },
   update: async (id, profile) => {
-    const response = await api.put(`/profile/${id}`, profile);
+    const response = await api.put(`/profiles/${id}`, profile);
     return response.data;
   }
 };
